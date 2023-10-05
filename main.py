b77817c1e41c5edaf0f88ddc0c9806d3df67f945
jurchiks33 <jurisslisans@gmail.com>
2023-10-05 17:51:54 +0200

    update

--- main.py
+++ main.py
@@ -12,5 +12,16 @@ def create_game_layout():
     ttk.Button(top_menu, text="Menu 2"). pack(side="left", padx=5)
     ttk.Button(top_menu, text="Menu 3"). pack(side="left", padx=5)
 
+    left_sidebar = ttk.Frame(root, width=150, relief="groove", padding=5)
+    left_sidebar.grid(row=1, column=0, rowspan=2, sticky="ns")
+    ttk.Button(left_sidebar, text="Item 1").pack(pady=5)
+    ttk.Button(left_sidebar, text="Item 2").pack(pady=5)
+    ttk.Button(left_sidebar, text="Item 3").pack(pady=5)
+
+    main_content = ttk.Frame(root, relief="groove", padding=5)
+    main_content.grid(row=1, column=1, sticky="nsew")
+    ttk.Label(main_content, text="Main Content Area", font=("Arial", 16)).pack(pady=20)
+
+    
 
     root.mainloop()
